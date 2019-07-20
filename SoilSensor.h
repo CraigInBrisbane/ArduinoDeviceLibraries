9be9029195ab23c19b9df28e0d63585ec09e3e0c
Craig Lister <craig@listerhome.com>
2019-07-20 14:33:43 +1000

    Adding Debug code

--- SoilSensor.h
+++ SoilSensor.h
@@ -5,6 +5,7 @@
         int Poll_Interval               = 0;
         int readValue                   = 0;
         unsigned long previousMillis    = 0;
+        bool debugMode                  = false;
 
     public:
 
@@ -20,17 +21,34 @@
             Poll_Interval = PollInterval;
         }
 
+        void SetDebugMode(bool DebugMode) {
+            debugMode = DebugMode;
+            if(DebugMode==true) {
+                Serial.begin(9600);
+                Serial.println("Debug mode enabled on SoilSensor...");
+            }
+        }
+
         // Method called to see if we have any updated from the sensor, based on the Poll Interval time.
         int Update() {
 
+
             // Are we ready to send updated?
-            if(Sensor_PIN == 0)
+            if(Sensor_PIN == 0) {
+                if(DebugMode==true) {
+                    Serial.println("Update called, but Init has not been called yet. Returning -1");
+                }
                 return -1;
+            }
 
             unsigned long currentMillis = millis();
 
             // Check if we're due a poll.
             if(currentMillis - previousMillis > Poll_Interval) {
+                if(DebugMode==true) {
+                    Serial.println("Will read...");
+                }
+
                 // Switch on the sensor.
                 digitalWrite(Power_PIN, HIGH);
                 // Let the sensor get ready for 20ms.
@@ -41,8 +59,21 @@
                 digitalWrite(Power_PIN, LOW);
                 // Record the last time a value was read.
                 previousMillis = currentMillis;
+
+                if(DebugMode==true) {
+                    Serial.print("Read value ");
+                    Serial.println(readValue);
+                }
+
                 // Return the last read value.
                 return readValue;
+            }
+
+            if(DebugMode == true) {
+                Serial.print("Update triggered, but poll value is ");
+                Serial.print(Poll_Interval);
+                Serial.print(", but difference is ");
+                Serial.println(currentMillis - previousMillis);
             }
         }
 };
