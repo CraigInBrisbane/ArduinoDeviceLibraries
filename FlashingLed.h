d9714d4e6823591ec84543ec15f1359cf4e4fe8a
Craig Lister <craig@listerhome.com>
2019-07-21 18:27:06 +1000

    Making flashingLed more like my other components

--- FlashingLed.h
+++ FlashingLed.h
@@ -2,14 +2,17 @@
  * A class that handles a flashing LED.
  */
 class FlashingLed {
-  int ledPin;
-  long onTime;
-  long offTime;
-  int state;
+  int   ledPin;
+  long  onTime;
+  long  offTime;
+  int   state;
   unsigned long previousMillis;
 
   public:
-  FlashingLed(int LedPin, long On, long Off) {
+  FlashingLed() {
+  }
+
+  void Init(int LedPin, long On, long Off) {
     ledPin = LedPin;
     onTime = On;
     offTime = Off;
@@ -18,21 +21,18 @@ class FlashingLed {
     previousMillis = 0;
   }
 
+
   void Update() {
     unsigned long currentMillis = millis();
     
     if((state == HIGH) and (currentMillis - previousMillis >= onTime)) {
       state = LOW;
-      //Serial.print("Going Low");
-      //Serial.println(state);
       previousMillis = currentMillis;
       digitalWrite(ledPin, state);
     } 
     else
     if((state==LOW) and (currentMillis - previousMillis >= offTime)) {
-      //Serial.println("Going High");
       state = HIGH;
-      //Serial.println(state);
       previousMillis = currentMillis;
       digitalWrite(ledPin, state);
     }
